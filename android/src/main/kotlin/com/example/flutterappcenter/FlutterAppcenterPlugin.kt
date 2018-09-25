336e518b0a8e1997cf0e8afc07eaca3e4f8c194f
Ethras <vladimirmbassi@gmail.com>
2018-09-26 00:08:38 +0200

    Starting android implementation

--- android/src/main/kotlin/com/example/flutterappcenter/FlutterAppcenterPlugin.kt
+++ android/src/main/kotlin/com/example/flutterappcenter/FlutterAppcenterPlugin.kt
@@ -1,25 +1,55 @@
 package com.example.flutterappcenter
 
+import android.app.Application
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.AppCenterService
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
-class FlutterAppcenterPlugin(): MethodCallHandler {
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar): Unit {
-      val channel = MethodChannel(registrar.messenger(), "flutter_appcenter")
-      channel.setMethodCallHandler(FlutterAppcenterPlugin())
-    }
-  }
 
-  override fun onMethodCall(call: MethodCall, result: Result): Unit {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+class FlutterAppcenterPlugin(private val app: Application) : MethodCallHandler {
+    var isConfigured = false
+    var appSecret: String = ""
+
+    companion object {
+        @JvmStatic
+        fun registerWith(registrar: Registrar): Unit {
+            val channel = MethodChannel(registrar.messenger(), "flutter_appcenter")
+            channel.setMethodCallHandler(FlutterAppcenterPlugin(registrar.activity().application))
+        }
     }
-  }
+
+    override fun onMethodCall(call: MethodCall, result: Result): Unit {
+        if (call.method.equals("getPlatformVersion")) {
+            result.success("Android ${android.os.Build.VERSION.RELEASE}")
+        } else {
+            result.notImplemented()
+        }
+    }
+
+    private fun configure(appSecret: String) {
+        this.appSecret = appSecret
+        isConfigured = true
+    }
+
+
+    private fun start(services: List<String>) {
+        val servicesClasses = arrayListOf<Class<out AppCenterService>>()
+        if (services.contains("analytics")) {
+            servicesClasses.add(Analytics::class.java)
+        }
+        if (services.contains("crashes")) {
+            servicesClasses.add(Crashes::class.java)
+        }
+
+        var servicesClassesArray = arrayOfNulls<Class<out AppCenterService>>(servicesClasses.count())
+        servicesClassesArray = servicesClasses.toArray(servicesClassesArray)
+
+        AppCenter.start(app, appSecret, *servicesClassesArray)
+    }
 }
